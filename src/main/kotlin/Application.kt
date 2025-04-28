7d3b6238667e0a709f33ac9890fce3fe841ae9d5
Radleigh123 <103168649+radleigh123@users.noreply.github.com>
2025-04-28 08:50:57 +0800

    initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,15 @@
+package com.capstone
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
